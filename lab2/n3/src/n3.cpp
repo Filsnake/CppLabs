f5fb70e04f6c1f255fff7803b8f97358c4df1226
Dmitry Reshetnikov <dimoncihik999@list.ru>
2017-10-31 17:35:49 +0500

    lab2

---
+++ lab2/n3/src/n3.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout <<"Введите размер массива: ";
+	cin >> n;
+
+	int *a = new int[n];
+	for (int i=0; i<n; i++)
+	{
+		cin >> a[i];
+	}
+
+    int max=a[0],
+    	min=a[0],
+		masmax=0,
+		masmin=0;
+
+    for(int i=1;i<n;i++)
+    {
+        if(max < a[i])
+        {
+        	masmax=i;
+            max=a[i];
+        }
+        if(min >= a[i])
+        {
+        	masmin=i;
+            min=a[i];
+        }
+    }
+    int x;
+    for(int i=masmax+1; i <masmin; i++)
+    {
+        for(int j=masmin-1; j>i; j--)
+        {
+            if(a[j]>a[j-1])
+            {
+            	x=a[j];
+               a[j]=a[j-1];
+                a[j-1]=x;
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<endl;
+    }
+	return 0;
+}
