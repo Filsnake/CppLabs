a8c5ceb4179bab751adbb7d4129de438046a20e9
Dmitry Reshetnikov <dimoncihik999@list.ru>
2017-10-31 09:25:39 +0500

    lab2

--- lab2/n2/src/n2.cpp
+++ lab2/n2/src/n2.cpp
@@ -1,38 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 	int n;
-	cout << "Введите длинну массива: ";
+	cout <<"Ent num: ";
 	cin >> n;
 
-	int* a = new int[n];
-	for (int i=0; i<n; i++)
+	int *a= new int[n];
+	for(int i=0; i<n; i++)
 		cin >> a[i];
 
 	int x;
-	for (int i=0; i<n; i++)
-	{
-		for (int i=0; i<n; i++)
+	for(int i=0; i<n; i++){
+		for(int i=0; i<n; i++)
 		{
-			if(a[i] < a[i+1] && a[i] ==0)
+			if(a[i - 1] < a[i] && a[i] == 0)
 			{
-				x = a[i];
-				a[i] = a[i+1];
-				a[i+1] = x;
+				x= a[i-1];
+			a[i-1] = a[i];
+			a[i] = x;
 			}
-			if (a[i] < a[i+1] && a[i]<0 && a[i+1] >0)
+			if(a[i-1] < a[i] && a[i-1] <0 && a[i] > 0)
 			{
-				x = a[i];
-				a[i] = a[i+1];
-				a[i+1] = x;
+				x=a[i-1];
+				a[i-1] = a[i];
+				a[i] = x;
 			}
 		}
 	}
-	for (int i = 0; i < n; i++)
-	{
-		cout << a[i] <<endl;
-	}
-	delete [] a;
+	for (int i=0; i<n; i++)
+	 cout << a[i] << endl;
 	return 0;
 }
