4e943ca872ae7b7f2314a37dd348a67589577113
Dmitry Reshetnikov <dimoncihik999@list.ru>
2017-11-02 10:12:29 +0500

    lab3

---
+++ lab3/n5/src/n5.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+
+	ofstream textFile("INPUT.txt");
+
+	for(int i=0; i<2; i++){
+		for(int i=0; i<5; i++)
+			textFile << rand()% 10<<' ';
+		textFile << endl;
+	}
+
+	textFile.close();
+
+	ofstream text("OUTPUT.txt");
+
+	int n,x,y;
+
+	list<int> vec;
+	list<int>::iterator it,it2;
+
+	ifstream file("INPUT.txt");
+
+
+
+	while(file>> n)
+		vec.push_back(n);
+
+	cout << "Элементы в файле INPUT.txt: " << endl;
+
+	for(it = vec.begin(); it != vec.end(); it++)
+		cout << *it;
+	cout << endl;
+
+	cout << "Элемент списка после которого вставить новый элемент: " << endl;
+	cin >> x;
+	cout << "Новый элемент: " << endl;
+	cin >> y;
+
+	it = vec.begin();
+	it2 = vec.end();
+	while(it != it2)
+		if (*it++ == x)
+			vec.insert(it,y);
+
+	for(it = vec.begin(); it != vec.end(); it++)
+		text << *it << ' ';
+
+	text.close();
+
+	file.close();
+
+	return 0;
+}
