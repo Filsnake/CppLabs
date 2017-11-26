189450503139deddc3fbc373f640c512577d6f63
Dmitry Reshetnikov <dimoncihik999@list.ru>
2017-11-26 14:09:24 +0500

    lab3

---
+++ lab3/n9.v2/n9.v2/n9.v2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ofstream file("ODD.txt");
+	ofstream filetext("EVEN.txt");
+	ifstream text("INPUT.txt");
+
+	char* buf = new char[255];
+	int* mas = new int[255];
+
+	while (!text.eof())
+	{
+		text.getline(buf, 255);
+
+		char *str = buf;
+
+		for (int y = 0, i = 0; i < 255; i++, y++)
+		{
+			if (str[y] == '\0')
+				break;
+			if (str[y] == ' ')
+				continue;
+			else
+			{
+				mas[i] = atoi(str + y);
+
+				if (mas[i] % 2 == 0)
+					filetext << mas[i] << " ";
+				else
+					file << mas[i] << " ";
+				if (mas[i] >= 10)
+					y++;
+				if (mas[i] < 0)
+					y++;
+			}
+
+		}
+
+	}
+	delete[] mas;
+	delete[] buf;
+
+	file.close();
+	text.close();
+	filetext.close();
+
+	return 0;
+}
