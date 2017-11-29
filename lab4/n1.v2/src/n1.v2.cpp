1af19f79b12e5fc2bdf065dc4e6327aaeca46958
Dmitry Reshetnikov <dimoncihik999@list.ru>
2017-11-29 14:04:40 +0500

    lab4

---
+++ lab4/n1.v2/src/n1.v2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+struct note
+{
+	char surname[30];
+	char name[30];
+	long int phone_number;
+	int date[3];
+};
+
+struct list
+{
+	note a;
+	list* next;
+};
+
+void init
+{
+
+}
+int main()
+{
+
+	list* begin=NULL;
+	return 0;
+}
